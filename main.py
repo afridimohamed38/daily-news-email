eaea08c552ed28a956838cdff5020f176f9ab9e9
Afridi <afridimohamed38@gmail.com>
2023-10-29 20:19:05 +0530

    Day27 : get only english data from api and send email

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
+from send_email import send_email
+from langdetect import detect
 
 api_key = "19379b53d074490f846dc7afb97f4ae8"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-09-29&" \
+url = "https://newsapi.org/v2/everything?q=tesla&from=2023-09-29language=en" \
       "sortBy=publishedAt&apiKey=19379b53d074490f846dc7afb97f4ae8"
 
 request = requests.get(url)
@@ -10,7 +12,22 @@ content = request.json()
 
 # print(type(content))
 
+raw_message = ""
+
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
-    print("\n")
+    if article['title'] is not None:
+        title_val = article['title'] + "\n"
+        raw_message += title_val
+
+        des_val = article['description'] + "\n"
+        raw_message += des_val
+
+inter_message = f"""\
+Subject : Daily news digest
+
+{raw_message}
+"""
+
+final_message = inter_message.encode("utf-8")
+send_email(final_message)
+# print(final_message)
---
+++ send_email.py
@@ -0,0 +1,25 @@
+import smtplib
+import ssl
+import os
+
+
+def send_email(input_message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "afridimohamed38@gmail.com"
+    password = os.getenv("APP_PASSWORD")
+
+    receiver = "afridimohamed38@gmail.com"
+
+    to_send_message = input_message
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, to_send_message)
+
+
+if __name__ == "__main__":
+    message = "Hey there\nuser@user.com"
+    send_email(message)
