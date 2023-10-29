a6160cb89c7f938359716c1b7cfd0c8d0f22956a
Afridi <afridimohamed38@gmail.com>
2023-10-29 20:34:48 +0530

    Day27 : Add links, limit news, control category

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 from send_email import send_email
-from langdetect import detect
 
 api_key = "19379b53d074490f846dc7afb97f4ae8"
+topic = "technology"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-09-29language=en" \
-      "sortBy=publishedAt&apiKey=19379b53d074490f846dc7afb97f4ae8"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-09-29" \
+      "sortBy=publishedAt&apiKey=19379b53d074490f846dc7afb97f4ae8&language=en"
 
 request = requests.get(url)
 content = request.json()
@@ -14,13 +14,13 @@ content = request.json()
 
 raw_message = ""
 
-for article in content['articles']:
+for article in content['articles'][0:20]:
     if article['title'] is not None:
         title_val = article['title'] + "\n"
         raw_message += title_val
 
-        des_val = article['description'] + "\n"
-        raw_message += des_val
+        des_url_val = article['description'] + "\n" + article['url'] + 2 * "\n"
+        raw_message += des_url_val
 
 inter_message = f"""\
 Subject : Daily news digest
